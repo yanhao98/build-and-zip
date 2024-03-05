e970d6320fec4e59566a2099a591bec71cddb767
严浩 <me@yanhao.ren>
2024-03-05 10:23:32 +0800

    Add dayjs library and update build script

--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
-import fs from "fs";
-import yargs from "yargs";
-import { execSync, exec } from 'child_process';
 import archiver from 'archiver';
-import os from 'os';
+import dayjs from 'dayjs';
+import { exec, execSync } from 'node:child_process';
+import fs from "node:fs";
+import os from 'node:os';
+import yargs from "yargs";
+
+const cwd = process.cwd();
+const packageJsonPath = `${cwd}/package.json`;
+if (!fs.existsSync(packageJsonPath)) {
+    console.error(`File not found: ${packageJsonPath}`);
+    process.exit(1);
+}
+let pkgName = require(packageJsonPath).name;
+pkgName = pkgName.replace('@', '').replace('/', '-');
+const BUILD_TIME = dayjs().format('YYYYMMDD_HHmmss');
+
+const argv = yargs()
+    .option('script', {
+        default: 'build',
+    })
+    .parseSync(process.argv);
+
+const command = `npm run ${argv.script}`;
+execSync(command, {
+    stdio: 'inherit',
+    cwd,
+    env: {
+        ...process.env,
+        VITE_BUILD_TIME: BUILD_TIME,
+        VUE_APP_BUILD_TIME: BUILD_TIME,
+    }
+});
 
 function zipFolderAsync(sourceDir: string, outPath: string): Promise<void> {
+    // console.debug(`sourceDir :>> `, sourceDir);
+    // console.debug(`outPath :>> `, outPath);
     // https://github.com/archiverjs/node-archiver?tab=readme-ov-file#quick-start
+
     return new Promise((resolve, reject) => {
         // 创建文件的写入流
         const output = fs.createWriteStream(outPath);
@@ -32,19 +63,6 @@ function zipFolderAsync(sourceDir: string, outPath: string): Promise<void> {
         // 完成归档（这将关闭流）
         archive.finalize();
     });
-}
-
-function getCurrentFormattedTime() {
-    let now = new Date();
-
-    let year = now.getFullYear();
-    let month = String(now.getMonth() + 1).padStart(2, '0');
-    let day = String(now.getDate()).padStart(2, '0');
-    let hours = String(now.getHours()).padStart(2, '0');
-    let minutes = String(now.getMinutes()).padStart(2, '0');
-    let seconds = String(now.getSeconds()).padStart(2, '0');
-
-    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
 }
 
 function openFolder(path: string) {
@@ -78,38 +96,11 @@ function openFolder(path: string) {
     });
 }
 
-const cwd = process.cwd();
-const packageJsonPath = `${cwd}/package.json`;
-if (!fs.existsSync(packageJsonPath)) {
-    console.error(`File not found: ${packageJsonPath}`);
-    process.exit(1);
-}
-const name = require(packageJsonPath).name;
-if (name === 'build-and-zip') process.exit(0);
-
-const BUILD_TIME = getCurrentFormattedTime();
-
-const argv = yargs()
-    .option('script', {
-        default: 'build',
-    }).parseSync(process.argv);
-
-const command = `npm run ${argv.script}`;
-execSync(command, {
-    stdio: 'inherit',
-    cwd,
-    env: {
-        ...process.env,
-        VITE_BUILD_TIME: BUILD_TIME,
-        VUE_APP_BUILD_TIME: BUILD_TIME,
-    }
-});
-
 const distZipPath = `${cwd}/dist-zip`;
 if (!fs.existsSync(distZipPath)) {
     fs.mkdirSync(distZipPath);
 }
-const filename = `${name}_${BUILD_TIME}.zip`;
+const filename = `${pkgName}_${BUILD_TIME}.zip`;
 await zipFolderAsync(`${cwd}/dist`, `${distZipPath}/${filename}`);
 console.log(`Zip file created: ${distZipPath}/${filename}`);
 openFolder(distZipPath);
